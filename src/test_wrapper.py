78c8c4aafee819edd49a79fd9a2bab46adb5893d
Robin Deits <robin.deits@gmail.com>
2015-12-17 19:37:00 -0500

    future print

--- src/test_wrapper.py
+++ src/test_wrapper.py
@@ -1,3 +1,4 @@
+from __future__ import print_function
 from wrapper import newAutoDiff, newAutoDiffLike, squareVector, squareMatrix
 import numpy as np
 
@@ -6,40 +7,40 @@ y = squareVector(x)
 assert np.allclose(y.T, np.power(x, 2))
 
 x = newAutoDiff(np.array([1.0, 3.0]), np.array([2.0, 5.0]))
-print x.value()
-print x
+print(x.value())
+print(x)
 assert (x.value() == np.array([[1.0, 3.0]]).T).all()
 assert (x.derivatives() == np.array([[2.0, 5.0]]).T).all()
 
 y = squareVector(x)
-print y.value()
+print(y.value())
 assert (y.value() == np.power(x.value(), 2)).all()
 assert (y.derivatives() == x.derivatives() * 2 * x.value()).all()
 
 z = x + y
-print z.value()
+print(z.value())
 assert (z.value() == x.value() + y.value()).all()
 assert (z.derivatives() == x.derivatives() + y.derivatives()).all()
 
 w = (((x + 1) - 5) * 2) / 1.5
-print w.value()
+print(w.value())
 assert (w.value() == (x.value() + 1 - 5) * 2 / 1.5).all()
 assert np.allclose(w.derivatives(), x.derivatives() * 2 / 1.5)
 
 q = x.arrayMultiply(x)
-print q.value()
+print(q.value())
 assert np.allclose(q.value(), y.value())
 assert np.allclose(q.derivatives(), y.derivatives())
 
 x = newAutoDiff(np.array([[1., 2.], [3., 4.]]), np.array([[3., 3., 3., 3.]]).T)
-print x.value()
+print(x.value())
 y = squareMatrix(x)
-print y.value()
+print(y.value())
 assert np.allclose(y.value(), np.power(x.value(), 2))
 assert np.allclose(y.derivatives(), 2 * x.value().reshape((-1,1), order='F') * x.derivatives())
 
 y = newAutoDiffLike(x, [1,3,2,4])
-print y.value()
+print(y.value())
 assert np.allclose(y.value(), x.value())
 w = x + y
 assert np.allclose(w.value(), x.value() + y.value())
